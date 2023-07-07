3a4f4e01cd2e7b1576fd4615aa5f1de8b6d3f952
Kieran Sweeden <kieransweeden@gmail.com>
2023-07-08 00:04:04 +0100

    Add geologica font family

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.css';
 
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 
@@ -20,12 +22,38 @@
 
 		return () => data.subscription.unsubscribe();
 	});
+
+	async function handleSignIn() {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'google'
+		});
+	}
+
+	async function handleSignOut() {
+		const { error } = await supabase.auth.signOut();
+	}
+
 </script>
 
 <svelte:head>
 	<title>User Management</title>
 </svelte:head>
 
-<div class="container" style="padding: 50px 0 100px 0">
+<AppShell>
+	<svelte:fragment slot="pageHeader">
+		<AppBar slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<a href="/">Bubbles</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				{#if session}
+					<a href="/account">Account</a>
+					<button class="btn btn-sm variant-filled" on:click={handleSignOut}>Sign out</button>
+				{:else}
+					<button class="btn btn-sm variant-filled" on:click={handleSignIn}>Sign in</button>
+				{/if}
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
 	<slot />
-</div>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { Auth } from '@supabase/auth-ui-svelte';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	
 
 	export let data;
 </script>
@@ -11,13 +10,6 @@
 
 <div class="row flex-center flex">
 	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			providers={['google']}
-			onlyThirdPartyProviders={true}
-            appearance={{ theme: ThemeSupa }}
-            theme='dark'
-            additionalData={{ }}
-		/>
+		
 	</div>
 </div>
